cc86a17c85a0ba1ee5cb0615a54fd8458ff03c35
Poom Wettayakorn <poom.wet@student.mahidol.ac.th>
2019-04-20 00:49:26 +0700

    added animations_with_matplotlib live_graph

---
+++ data_visualization/animations_with_matplotlib/live_graph.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import pandas as pd
+
+# create figure
+fig = plt.figure(figsize=(10,10))
+ax = fig.add_subplot(1,1,1)
+
+def live_update(i):
+    # load data
+    df = pd.DataFrame.from_csv('live_graph_stock.csv', index_col=None)
+
+    xs = df['Date'].values
+    ys = df['Price'].values
+   
+    # visualize
+    ax.clear()
+    ax.plot(xs, ys)
+
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Live Stock Chart with Matplotlib')	
+    
+# interval -> Delay between frames in milliseconds
+ani = animation.FuncAnimation(fig, live_update, interval=500) 
+plt.show()
